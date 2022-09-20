acf724e122243f68f62ac1ee721d8b0a8338121f
anadidev <anadi.profile@gmail.com>
2022-09-21 02:18:58 +0530

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,46 @@
+﻿using Confluent.Kafka;
+
+using Consumer;
+
+using System.Text.Json;
+
+string topic = "test";
+string groupId = "test_group";
+string bootstrapServers = "localhost:9092";
+
+var config = new ConsumerConfig
+{
+    GroupId = groupId,
+    BootstrapServers = bootstrapServers,
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+try
+{
+    using (var consumerBuilder = new ConsumerBuilder
+    <Ignore, string>(config).Build())
+    {
+        consumerBuilder.Subscribe(topic);
+        var cancelToken = new CancellationTokenSource();
+
+        try
+        {
+            while (true)
+            {
+                var consumer = consumerBuilder.Consume
+                   (cancelToken.Token);
+                var orderRequest = JsonSerializer.Deserialize<OrderProcessingRequest>(consumer.Message.Value);
+                Console.WriteLine($"Processing Order: {consumer.Message.Value}");
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumerBuilder.Close();
+        }
+    }
+}
+catch (Exception ex)
+{
+    System.Diagnostics.Debug.WriteLine(ex.Message);
+}
+Console.ReadLine();
---
+++ Producer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+
+using Producer;
+
+using System.Net;
+using System.Text.Json;
+
+string bootstrapServers = "localhost:9092";
+string topic = "test";
+
+ProducerConfig config = new ProducerConfig
+{
+    BootstrapServers = bootstrapServers,
+    ClientId = Dns.GetHostName()
+};
+
+try
+{
+    OrderRequest order = new OrderRequest
+    {
+        OrderId = 1,
+        ProductId = 2,
+        CustomerId = 3,
+        Quantity = 10,
+        Status = "Orderd"
+    };
+    using (var producer = new ProducerBuilder<Null, string>(config).Build())
+    {
+        var result = await producer.ProduceAsync(topic, new Message<Null, string>
+        {
+            Value = JsonSerializer.Serialize(order)
+        });
+
+        Console.WriteLine($"Delivery Timestamp: {result.Timestamp.UtcDateTime}");
+        Console.WriteLine($"Message : {result.Value}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error occured: {ex.Message}");
+}
+
+Console.ReadLine();
+
